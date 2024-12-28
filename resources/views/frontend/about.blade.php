a4d01a00da83008c781a6c8307c9d2adebfd1842
Vannak Dim <vannakhannah@gmail.com>
2024-12-28 11:47:53 +0700

    modify progress bar

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -102,10 +102,7 @@
 
             <div class="section-title" data-aos="fade-up">
                 <h2>Our <strong>Skills</strong></h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                    consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
-                    sit
-                    in iste officiis commodi quidem hic quas.</p>
+                <p>ខាងក្រោមនេះជាបច្ចេកវិទ្យាដែលយើងខ្ញុំប្រើប្រាស់ក្នុងការផលិតគេហទំព័រជូនលោកអ្នក</p>
             </div>
 
             <div class="row skills-content">
@@ -144,7 +141,7 @@
                 <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
 
                     <div class="progress">
-                        <span class="skill">PHP <i class="val">80%</i></span>
+                        <span class="skill">PHP/Laravel <i class="val">80%</i></span>
                         <div class="progress-bar-wrap">
                             <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0"
                                 aria-valuemax="100">
@@ -153,7 +150,7 @@
                     </div>
 
                     <div class="progress">
-                        <span class="skill">WordPress/CMS <i class="val">90%</i></span>
+                        <span class="skill">CMS <i class="val">90%</i></span>
                         <div class="progress-bar-wrap">
                             <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
                                 aria-valuemax="100">
@@ -162,7 +159,7 @@
                     </div>
 
                     <div class="progress">
-                        <span class="skill">Photoshop <i class="val">55%</i></span>
+                        <span class="skill">React <i class="val">55%</i></span>
                         <div class="progress-bar-wrap">
                             <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
                                 aria-valuemax="100">
