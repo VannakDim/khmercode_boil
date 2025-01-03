9d196d66065062d4446bbc601d94bd54cc6b9ac5
Vannak Dim <vannakhannah@gmail.com>
2025-01-03 10:14:30 +0700

    sort by categories

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,9 +15,6 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('all.brand') }}" :active="request()->routeIs('all.brand')">
-                        {{ __('Brand') }}
-                    </x-nav-link>
                 </div>
               
             </div>
