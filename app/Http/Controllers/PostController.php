f1f2e1e15609a3e33c71e18bd454ca64196ebbf5
Vannak Dim <vannakhannah@gmail.com>
2025-01-03 09:33:20 +0700

    sort by categories

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::where('status', 'public')->get()->sortByDesc('created_at');
         return view('admin.post.index', compact('posts'));
     }
     public function page_add()
